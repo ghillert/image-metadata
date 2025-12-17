6fbfec7262b1e2a4b4c64e45c8b3d447d9aa8f38
Gunnar Hillert <gunnar@hillert.com>
2025-12-17 12:27:42 -1000

    Add AI Spring Boot configuration properties for image description generation

--- image-metadata-commons-imaging/src/main/java/com/hillert/image/metadata/config/MetadataConfigProperties.java
+++ image-metadata-commons-imaging/src/main/java/com/hillert/image/metadata/config/MetadataConfigProperties.java
@@ -10,12 +10,52 @@ public class MetadataConfigProperties {
 	 */
 	private Boolean doNotResize;
 
+	private Ai ai;
+
 	public Boolean getDoNotResize() {
 		return this.doNotResize;
 	}
 
 	public void setDoNotResize(Boolean doNotResize) {
 		this.doNotResize = doNotResize;
+	}
+
+	public Ai getAi() {
+		return this.ai;
+	}
+
+	public void setAi(Ai ai) {
+		this.ai = ai;
+	}
+
+	public static class Ai {
+
+		/**
+		 * If true, create an image description using AI.
+		 */
+		private Boolean createImageDescription;
+
+		/**
+		 * The AI text prompt to use to generate the image description.
+		 */
+		private String textPrompt;
+
+		public Boolean getCreateImageDescription() {
+			return this.createImageDescription;
+		}
+
+		public void setCreateImageDescription(Boolean createImageDescription) {
+			this.createImageDescription = createImageDescription;
+		}
+
+		public String getTextPrompt() {
+			return this.textPrompt;
+		}
+
+		public void setTextPrompt(String textPrompt) {
+			this.textPrompt = textPrompt;
+		}
+
 	}
 
 }
